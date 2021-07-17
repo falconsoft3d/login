412243ad101823560413f3f8fc38c26dd856dba8
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-07-17 13:35:12 +0200

    sc

--- nodejs-mysql-es6/src/index.js
+++ nodejs-mysql-es6/src/index.js
@@ -1,58 +1,65 @@
 const express = require('express');
 const morgan = require('morgan');
-const exphbs = require('express-handlebars');
 const path = require('path');
-const flash = require('connect-flash');
+const exphbs = require('express-handlebars');
 const session = require('express-session');
-const MySQLStore = require('express-mysql-session');
-const { database } = require('./keys')
+const validator = require('express-validator');
+const passport = require('passport');
+const flash = require('connect-flash');
+const MySQLStore = require('express-mysql-session')(session);
+const bodyParser = require('body-parser');
 
-// init 
+const { database } = require('./keys');
+
+// Intializations
 const app = express();
+require('./lib/passport');
 
-
-// settings
+// Settings
 app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'),'layouts'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
-}));
-
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}))
 app.set('view engine', '.hbs');
 
+// Middlewares
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
-// Middleware
 app.use(session({
-    secret: 'asasssa-session',
-    resave: false,
-    saveUninitialized: false,
-    store: new MySQLStore(database)
+  secret: 'faztmysqlnodemysql',
+  resave: false,
+  saveUninitialized: false,
+  store: new MySQLStore(database)
 }));
 app.use(flash());
-app.use(morgan('dev'));
-app.use(express.urlencoded( {extended: false} ));
-app.use(express.json());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(validator());
 
-
-// Global Variables
+// Global variables
 app.use((req, res, next) => {
-    app.locals.success = req.flash('success')
-    next(); 
+  app.locals.message = req.flash('message');
+  app.locals.success = req.flash('success');
+  app.locals.user = req.user;
+  next();
 });
 
-
 // Routes
-app.use(require('./routes'));
-app.use(require('./routes/authentications'));
+app.use(require('./routes/index'));
+app.use(require('./routes/authentication'));
 app.use('/links', require('./routes/links'));
 
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Starting the Server
-app.listen(app.get('port'), ()=>{
-    console.log('Server on port: ', app.get('port'))
+// Starting
+app.listen(app.get('port'), () => {
+  console.log('Server is in port', app.get('port'));
 });
--- nodejs-mysql-es6/src/keys.js
+++ nodejs-mysql-es6/src/keys.js
@@ -1,8 +1,11 @@
 module.exports = {
-    database : {
+
+    database: {
+        connectionLimit: 10,
         host: 'localhost',
         user: 'root',
         password: 'x1234567890',
         database: 'database_links'
     }
+
 };
